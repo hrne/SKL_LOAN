c433b55e4a495537a9ba03a36a96f8a1321e121a
xiangwei <xiangwei326@gmail.com>
2022-06-10 17:48:47 +0800

    Auto-committed on 2022/06/10 週五 17:48:45.69

--- Program/PT2_iTX/src/main/java/com/st1/itx/db/domain/LoanBorTx.java
+++ Program/PT2_iTX/src/main/java/com/st1/itx/db/domain/LoanBorTx.java
@@ -2,6 +2,7 @@ package com.st1.itx.db.domain;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.sql.Time;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.EntityListeners;
@@ -26,12 +27,7 @@ import com.st1.itx.Exception.LogicException;
 public class LoanBorTx implements Serializable {
 
 
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 9119226293997969828L;
-
-@EmbeddedId
+  @EmbeddedId
   private LoanBorTxId loanBorTxId;
 
   // 借款人戶號
