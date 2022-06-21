cdd9dc9ae967c45f7c3fe4905d74675ac36f2ffa
xiangwei <xiangwei326@gmail.com>
2022-06-21 17:50:30 +0800

    Auto-committed on 2022/06/21 週二 17:50:27.88

--- Program/PT2_iTX/src/main/java/com/st1/itx/trade/LY/LY004.java
+++ Program/PT2_iTX/src/main/java/com/st1/itx/trade/LY/LY004.java
@@ -1,34 +1,45 @@
 package com.st1.itx.trade.LY;
 
-import java.util.ArrayList;
-
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.st1.itx.Exception.LogicException;
-import com.st1.itx.dataVO.TitaVo;
-import com.st1.itx.dataVO.TotaVo;
-import com.st1.itx.tradeService.TradeBuffer;
-import com.st1.itx.util.MySpring;
+import com.st1.itx.tradeService.BatchBase;
 
 @Service("LY004")
-@Scope("prototype")
+@Scope("step")
 /**
  * 
  * 
  * @author Ted Lin
  * @version 1.0.0
  */
-public class LY004 extends TradeBuffer {
+public class LY004 extends BatchBase implements Tasklet, InitializingBean {
+
+	@Autowired
+	LY004Report ly004report;
 
 	@Override
-	public ArrayList<TotaVo> run(TitaVo titaVo) throws LogicException {
-		this.info("active LY004 ");
-		this.totaVo.init(titaVo);
-
-		MySpring.newTask("LY004p", this.txBuffer, titaVo);
-
-		this.addList(this.totaVo);
-		return this.sendList();
+	public void afterPropertiesSet() throws Exception {
+		;
 	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		return this.exec(contribution, "M");
+	}
+
+	@Override
+	public void run() throws LogicException {
+		this.info("active LY004 ");
+		ly004report.setTxBuffer(this.getTxBuffer());
+		ly004report.exec(titaVo);
+	}
+
 }
