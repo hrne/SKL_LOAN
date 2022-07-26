8d82037168f9498eb2bed18eed52c553a7392f9b
xiangwei <xiangwei326@gmail.com>
2022-07-26 17:41:46 +0800

    Auto-committed on 2022/07/26 週二 17:41:45.60

--- Program/PT1_iTX/src/main/java/com/st1/itx/trade/LM/LM027Report.java
+++ Program/PT1_iTX/src/main/java/com/st1/itx/trade/LM/LM027Report.java
@@ -142,7 +142,7 @@ public class LM027Report extends MakeReport {
 					custNo = String.format("%07d", Integer.parseInt(tLM027Vo.get("F0")));
 //					this.info("custNo=" + custNo); 65000 64000
 
-					makeExcel.setValue(row, 1, Integer.parseInt(tLM027Vo.get("F0")), "C");
+					makeExcel.setValue(row, 1, Integer.parseInt(tLM027Vo.get("F0")),"0000000", "C");
 
 					makeExcel.setValue(row, 2, tLM027Vo.get("F1"), "C");
 
