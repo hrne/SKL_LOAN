9198079561a0cd512c80cf49b47356f2f05e5a71
Adam <doom167.abc@msa.hinet.net>
2023-05-22 14:41:25 +0800

    20230522(Adam v2

--- Program/war/iFX/SKL_Sql.sql
+++ Program/war/iFX/SKL_Sql.sql
@@ -602,6 +602,23 @@ CREATE UNIQUE INDEX "FX_SWIFT_PRINTER_IDX" ON webadmin."FX_SWIFT_PRINTER" ("BRN"
 ALTER TABLE webadmin."FX_SWIFT_PRINTER"
 ADD  CONSTRAINT "FX_SWIFT_PRINTER_PK" PRIMARY KEY ("BRN");
 --------------------------------------------------------------------------------------------------------------------------------
+--FTP_USER
+CREATE TABLE itxadmin."FTP_USER" (      
+   userid VARCHAR2(64) NOT NULL,       
+   userpassword VARCHAR2(64),      
+   homedirectory VARCHAR2(128) NOT NULL,             
+   enableflag VARCHAR2(1) DEFAULT 'Y',    
+   writepermission VARCHAR2(1) DEFAULT 'N',       
+   idletime NUMBER(19,0) DEFAULT 0,             
+   uploadrate NUMBER(19,0) DEFAULT 0,             
+   downloadrate NUMBER(19,0) DEFAULT 0,
+   maxloginnumber NUMBER(19,0) DEFAULT 0,
+   maxloginperip NUMBER(19,0) DEFAULT 0
+);
+
+ALTER TABLE itxadmin."FTP_USER"
+ADD  CONSTRAINT "FTP_USER_PK" PRIMARY KEY ("USERID");
+--------------------------------------------------------------------------------------------------------------------------------
 --SpringBatch
 --1、批量實例表
 CREATE TABLE webadmin.FX_BATCH_JOB_INSTANCE  (
