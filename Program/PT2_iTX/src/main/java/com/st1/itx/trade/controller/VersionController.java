c5926d0fdb43b7ea905c60db12b80e3490362fa6
St1Server <st1.server.01@gmail.com>
2023-06-28 18:10:03 +0800

    Auto-committed on 2023/06/28 週三 18:10:01.04

---
+++ Program/PT2_iTX/src/main/java/com/st1/itx/trade/controller/VersionController.java
@@ -0,0 +1,41 @@
+package com.st1.itx.trade.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.st1.itx.web.TradeController;
+
+@Controller
+public class VersionController extends TradeController {
+
+	@Value("${appVersion}")
+	private String appVersion;
+
+	@Override
+	@PostConstruct
+	public void init() {
+		super.mustInfo("VersionController Init....");
+	}
+
+	/* method = { RequestMethod.POST, RequestMethod.GET } */
+
+	// http://localhost:8080/iTX/mvc/trade/VS
+	@RequestMapping(value = "VS", method = { RequestMethod.GET })
+	@ResponseBody
+	public ResponseEntity<String> testSec(HttpServletResponse response) {
+		this.mustInfo("VersionController appVersion = " + appVersion);
+		Map<String, String> map = new HashMap<>();
+		map.put("appVersion", appVersion);
+		return this.makeJsonResponse(map, true);
+	}
+}
