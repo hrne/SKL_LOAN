09e098470ebb9e0c611e78afa7be9dac38592a8e
St1Server <st1.server.01@gmail.com>
2023-10-12 18:58:14 +0800

    Wei clean garbage

--- Program/Other/change_CBC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak  
-
-sudo sed -i '$a\Ciphers aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com' /etc/ssh/sshd_config
-
-sudo service ssh restart
